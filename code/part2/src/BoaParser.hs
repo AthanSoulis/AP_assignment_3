56cecd0a6bb3cb3646f79057fdf985d3725401b2
Thanos Soulis <athan.soulis@gmail.com>
2022-09-29 18:27:42 +0200

    String const with merges

--- code/part2/src/BoaParser.hs
+++ code/part2/src/BoaParser.hs
@@ -6,25 +6,23 @@ import BoaAST
 -- add any other other imports you need
 
 import Text.ParserCombinators.ReadP
-import Control.Applicative ((<|>))
-  -- may use instead of +++ for easier portability to Parsec
-
-import Data.Char (isDigit, isSpace, isLetter)
+import Control.Applicative ((<|>)) -- may use instead of +++ for easier portability to Parsec
+import Data.Char (isDigit, isSpace, isLetter, isPrint)
 
 type Parser a = ReadP a   -- may use synomym for easier portability to Parsec
 type ParseError = String -- you may replace this
 
 reservedIdents = ["None", "True", "False", "for", "if", "in", "not"]
 
--- parseString :: String -> Either ParseError Program --Program
+-- parseString :: String -> Either ParseError Program
 -- parseString s  = case [a | (a,t) <- readP_to_S parseProgram s, all isSpace t] of
---               [a] -> Right a--Right a
+--               [a] -> Right a
 --               [] -> Left "Parsing failed" 
 --               _ -> Left "How did it get here ?"
 
-parseString :: String -> Either ParseError Program --Program
-parseString s  = case [a | (a,t) <- readP_to_S parseConst s, all isSpace t] of
-              [a] -> Right [SExp a]--Right a
+parseString :: String -> Either ParseError Program
+parseString s  = case [a | (a,t) <- readP_to_S parseStringConst s, all isSpace t] of
+              [a] -> Right [SExp a]
               [] -> Left "Parsing failed" 
               _ -> Left "How did it get here ?"
 
@@ -41,24 +39,23 @@ parseStmts = do
 
 parseStmts' :: [Stmt] -> Parser [Stmt]
 parseStmts' prevStmts = do
-                            satisfy(==';')
-                            skipWS
-                            parseStmts' prevStmts
+                        satisfy(==';')
+                        skipWS
+                        parseStmts' prevStmts
                         <|>
-                            return prevStmts
+                        return prevStmts
 
 parseStmt :: Parser Stmt
 parseStmt = do
-                ident <- parseIdent
-                skipWS
-                satisfy(=='=')
-                skipWS
+            ident <- parseIdent
+            skipWS
+            satisfy(=='=')
+            skipWS
+            expr <- parseExpr
+            return $ SExp expr
+            <|>do 
                 expr <- parseExpr
                 return $ SExp expr
-            <|>
-                do 
-                    expr <- parseExpr
-                    return $ SExp expr
 
 parseIdent :: Parser String
 -- parseIdent :: Parser Either VName FName
@@ -72,7 +69,7 @@ parseExpr = do
                 skipWS       -- munch1 isWhitespace
                 parseExpr
             <|>
-                parseRel
+            parseRel
 
 
 parseRel :: Parser Exp
@@ -82,14 +79,32 @@ parseRel = do
 
 parseRel' :: Exp -> Parser Exp
 parseRel' expr = do
-                parseRelOper
+                parseRelOper expr
                 parseAddNeg
-            <|>
+                <|>
                 return expr
 
-parseRelOper :: Parser Op
-parseRelOper = undefined
-
+parseRelOper :: Exp -> Parser Op
+parseRelOper expr = undefined 
+                -- do
+                -- string "=="
+                -- return Eq 
+                -- <|> do
+                -- string "!="
+                -- return Not Eq
+                -- <|> do
+                -- satisfy (== '<')
+                -- <|> do
+                -- satisfy (== '>')
+                -- return 
+                -- <|> do
+                -- string "<="
+                -- <|> do
+                -- string ">="
+                -- <|> do
+                -- string "in"
+                -- <|> do
+                -- string "not in"
 parseAddNeg :: Parser Exp
 parseAddNeg = undefined
 
@@ -165,6 +180,13 @@ parseExprs = undefined
 parseExprs' = undefined
 
 
+-- To be extended,  probably try to consume '#'  and move on from there
+skipWS :: Parser ()
+skipWS = skipSpaces
+
+parseComments :: Parser ()
+parseComments = undefined
+        
 
 parseNumConst :: Parser Exp
 parseNumConst = do
@@ -183,23 +205,33 @@ parseNumConstHelper = do
         '0' -> if length num == 1 then return 0 else pfail 
         _  -> return $ read num
 
+parseStringConst :: Parser Exp
+parseStringConst = do
+    satisfy (== '\'')
+    skipWS
+    print <- many parseStringInside
+    satisfy (== '\'')
+    skipWS
+    return $ Const (StringVal $ concat $ print)
+
+parseStringInside :: Parser String
+parseStringInside = do 
+                    c <- satisfy isPrintable
+                    return (c:[])
+                    <|> do
+                    string "\\\n"
+                    return ""
+                    <|> do
+                    string "\n"
+                    return "\n"
+                    <|> do
+                    string "\'"
+                    return "\'"
+                    <|> do
+                    string "\\\\"
+                    return "\\"
 
 
--- To be extended,  probably try to consume '#'  and move on from there
-skipWS :: Parser ()
-skipWS = skipSpaces
+isPrintable :: Char -> Bool
+isPrintable c = ((isPrint c) && (c /= '\'') && (c /= '\\'))
 
-
-
-
--- parseNumConst = do
---     num <- munch1 isDigit
---     case (head num) of
---         '0' -> if length num == 1 then return $ Const (IntVal 0) else pfail 
---         _  -> return $ Const (IntVal $ read num)
---     <|> do 
---     satisfy (== '-')
---     num <- munch1 isDigit
---     case (head num) of
---         '0' -> if length num == 1 then return $ Const (IntVal 0) else pfail
---         _   -> return $ Const (IntVal $ read ("-"++num))
