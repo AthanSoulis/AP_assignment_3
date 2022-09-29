560f9a5f660eec6a6b86b37aa1603c02144c2517
Thanos Soulis <athan.soulis@gmail.com>
2022-09-29 13:58:50 +0200

    Merge implementations

--- code/part2/src/BoaParser.hs
+++ code/part2/src/BoaParser.hs
@@ -15,29 +15,84 @@ type Parser a = ReadP a   -- may use synomym for easier portability to Parsec
 type ParseError = String -- you may replace this
 
 parseString :: String -> Either ParseError Program
--- parseString = undefined  -- define this
--- parseString s = case [a | (a,t) <- readP_to_S parseNumConst s, all isSpace t] of
---               [a] -> Right [SExp a]
---               [] -> Left "Parsing failed"
---               _ -> Left "How did it get here ?"
-
--- parseIdent :: Parser 
+parseString s  = case [a | (a,t) <- readP_to_S parseProgram s, all isSpace t] of
+              [a] -> Right a
+              [] -> Left "Parsing failed" 
+              _ -> Left "How did it get here ?"
 
 
+parseProgram :: Parser Program
+parseProgram = do
+                skipWS
+                parseStmts
+
+parseStmts :: Parser [Stmt]
+parseStmts = do 
+                stmt <- parseStmt
+                parseStmts' [stmt]
+
+parseStmts' :: [Stmt] -> Parser [Stmt]
+parseStmts' prevStmts = do
+                            satisfy(==';')
+                            skipWS
+                            parseStmts' prevStmts
+                        <|>
+                            return prevStmts
+
+parseStmt :: Parser Stmt
+parseStmt = do
+                ident <- parseIdent
+                skipWS
+                satisfy(=='=')
+                skipWS
+                expr <- parseExpr
+                return $ SExp expr
+            <|>
+                do 
+                    expr <- parseExpr
+                    return $ SExp expr
+
+parseIdent :: Parser String
+-- parseIdent :: Parser Either VName FName
+parseIdent = undefined
+
+parseExpr :: Parser Exp
+parseExpr = do
+                satisfy(=='n')
+                -- satisfy(=="not") 
+                skipWS
+                parseExpr
+            <|>
+                parseRel
 
 
-parseNumConst :: Parser Exp
--- parseNumConst = do
---     satisfy (== '-')
---     parseNumConstHelper true
---     <|> do
---     parseNumConstHelper false
---     where parseNumConstHelper minus = do
---         num <- munch1 isDigit
---         case (head num) of
---             '0' -> if length num == 1 then return $ Const (IntVal 0) else pfail 
---             _  -> return $ Const (IntVal if minus then read ("-"++num) else read num)
-    
+parseRel :: Parser Exp
+parseRel = do
+            e <- parseAddNeg
+            parseRel' e
+
+parseRel' :: Exp -> Parser Exp
+parseRel' expr = do
+                parseRelOper
+                parseAddNeg
+            <|>
+                return expr
+
+parseRelOper :: Parser Op
+parseRelOper = undefined
+
+parseAddNeg :: Parser Exp
+parseAddNeg = undefined
+
+parseConst :: Parser Exp
+parseConst = undefined
+
+-- To be extended,  probably try to consume '#'  and move on from there
+skipWS :: Parser ()
+skipWS = skipSpaces
+        
+
+
 
 parseNumConst = do
     num <- munch1 isDigit
