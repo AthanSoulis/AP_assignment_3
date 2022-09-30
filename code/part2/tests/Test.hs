e79fb328d48ac61bc9ddcc78e1c90fb14e47bda6
Thanos Soulis <athan.soulis@gmail.com>
2022-09-30 11:03:24 +0200

    Start of testing suite

--- code/part2/tests/Test.hs
+++ code/part2/tests/Test.hs
@@ -9,7 +9,9 @@ import Test.Tasty.HUnit
 main :: IO ()
 main = defaultMain $ localOption (mkTimeout 1000000) tests
 
-tests = testGroup "Minimal tests" [
+tests = testGroup "Test Suite" [min_tests, ass_description_tests]
+
+min_tests = testGroup "Minimal tests" [
   testCase "simple success" $
     parseString "2 + two" @?=
       Right [SExp (Oper Plus (Const (IntVal 2)) (Var "two"))],
@@ -18,3 +20,10 @@ tests = testGroup "Minimal tests" [
     case parseString "wow!" of
       Left e -> return ()  -- any message is OK
       Right p -> assertFailure $ "Unexpected parse: " ++ show p]
+
+ass_description_tests = testGroup "Assignment Description tests" [
+  
+  testCase "Nested not" $
+    case parseString "not (not (x < 3))" of
+      Left e -> assertFailure $ "Error: " ++ e
+      Right p -> return ()]
